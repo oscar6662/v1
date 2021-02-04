d4bcb1614af9cd0ab9f702cb169b1d3d43e90b73
oscar6662 <oscar.berenguer6@gmail.com>
2021-02-04 02:37:06 +0000

    lint fix

--- src/app.js
+++ src/app.js
@@ -1,41 +1,31 @@
-const lib = require("./videos");
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const lib = require('./videos');
+
 const app = express();
 const port = 3000;
 
-app.use('/css',express.static(path.join(__dirname, '../css')));
-app.use('/videos',express.static(path.join(__dirname, '../public/videos')));
+app.use('/css', express.static(path.join(__dirname, '../css')));
+app.use('/videos', express.static(path.join(__dirname, '../public/videos')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
-app.get('/:userQuery',(req,res)=>{
-  if(req.params.userQuery == 'index'){
-
-      let video = lib.getRqst();
-      
-      res.render('index',{video:video});
- 
-    
-  }else{
-    res.status(404).send('what?')
+app.get('/:userQuery', (req, res) => {
+  if (req.params.userQuery === 'index') {
+    const video = lib.getRqst();
+    res.render('index', { video });
+  } else {
+    res.status(404).send('what?');
   }
+});
 
-}); 
-app.get('/video/:userQuery',(req,res)=>{
-  let nr= req.params.userQuery;
-  let video = lib.getRqst();
-  let tengd = lib.tengd(nr);
-  console.log(tengd);
-  if(lib.avlvideos(nr)){      
-      res.render('video',{video:video,id:nr,tengd:tengd});
-  }else{
-    res.status(404).send('what?')
+app.get('/video/:userQuery', (req, res) => {
+  const nr = req.params.userQuery;
+  const video = lib.getRqst();
+  const tengd = lib.tengdf(nr);
+  if (lib.avlvideos(nr)) {
+    res.render('video', { video, id: nr, tengd });
+  } else {
+    res.status(404).send('what?');
   }
-
-}); 
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+app.listen(port);
--- src/videos.js
+++ src/videos.js
@@ -1,32 +1,30 @@
 const fs = require('fs');
-let videos = JSON.parse(fs.readFileSync('videos.json', 'utf8'))
-function getRqst(){
+
+const videos = JSON.parse(fs.readFileSync('videos.json', 'utf8'));
+
+function getRqst() {
   return videos;
 }
 
-function avlvideos(nr){
-  for (var i = 0; i < videos.videos.length; i++) {
-      if(videos.videos[i].id==nr){
-        return true;
-      }
+function avlvideos(nr) {
+  for (let i = 0; i < videos.videos.length; i += 1) {
+    if (videos.videos[i].id === nr) {
+      return true;
+    }
   }
   return false;
 }
 
-function tengd(nr){
-  var tengd = new Array();
-  for(var i = 0; i<videos.videos[nr].related.length; i++){
-    tengd.push(videos.videos[nr].related[i]);
-    tengd.push(time(videos.videos[nr].related[i]));
-  } 
-  return tengd;
-}
-
-function time(nr){
-  var time = new Date().getTime();
-  var years= 0, months= 0, weeks= 0, days= 0, hours= 0,minutes = 0;
-  var seconds = videos.videos[nr].duration;
-  var timeCreated = time - videos.videos[nr].created;
+function timef(nr) {
+  const time = new Date().getTime();
+  let years = 0;
+  let months = 0;
+  let weeks = 0;
+  let days = 0;
+  let hours = 0;
+  let minutes = 0;
+  let seconds = videos.videos[nr].duration;
+  const timeCreated = time - videos.videos[nr].created;
 
   if (timeCreated / 3.154e10 >= 1) {
     years = Math.floor(timeCreated / 3.154e10);
@@ -41,39 +39,42 @@ function time(nr){
   }
   if (seconds / 60 >= 1) {
     minutes = Math.floor(seconds / 60);
-    seconds = seconds - 60 * minutes;
+    seconds -= 60 * minutes;
   }
-  if (years != 0) {
-    if (years == 1) {
-      return `Fyrir 1 ári síðan`;
-    } else {
-      return 'Fyrir '+years+' árum síðan';
+  if (years !== 0) {
+    if (years === 1) {
+      return 'Fyrir 1 ári síðan';
     }
-  } else if (months != 0) {
-    if (months == 1) {
-      return `Fyrir 1 mánuði síðan`;
-    } else {
-      return 'Fyrir '+ months +' mánuðum síðan';
+    return `Fyrir ${years} árum síðan`;
+  } if (months !== 0) {
+    if (months === 1) {
+      return 'Fyrir 1 mánuði síðan';
     }
-  } else if (weeks != 0) {
-    if (weeks == 1) {
-      return `Fyrir 1 viku síðan`;
-    } else {
-      return 'Fyrir '+ weeks+' vikum síðan';
+    return `Fyrir ${months} mánuðum síðan`;
+  } if (weeks !== 0) {
+    if (weeks === 1) {
+      return 'Fyrir 1 viku síðan';
     }
-  } else if (days != 0) {
-    if (days == 1) {
-      return `Fyrir 1 degi síðan`;
-    } else {
-      return 'Fyrir '+ days +' dögum síðan';
+    return `Fyrir ${weeks} vikum síðan`;
+  } if (days !== 0) {
+    if (days === 1) {
+      return 'Fyrir 1 degi síðan';
     }
-  } else {
-    if (hours == 1) {
-      return `Fyrir 1 klukkutíma síðan`;
-    } else {
-      return 'Fyrir '+hours+' klukkustundum síðan';
-    }
+    return `Fyrir ${days} dögum síðan`;
   }
+  if (hours === 1) {
+    return 'Fyrir 1 klukkutíma síðan';
+  }
+  return `Fyrir ${hours} klukkustundum síðan`;
 }
 
-module.exports = {getRqst,avlvideos,tengd};
+function tengdf(nr) {
+  const tengd = [];
+  for (let i = 0; i < videos.videos[nr].related.length; i += 1) {
+    tengd.push(videos.videos[nr].related[i]);
+    tengd.push(timef(videos.videos[nr].related[i]));
+  }
+  return tengd;
+}
+
+module.exports = { getRqst, avlvideos, tengdf };
